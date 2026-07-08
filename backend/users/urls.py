6606fad46c9a75e31cda0bb7ae49af280f178218
yunseol12334 <yunseol2132@gmail.com>
2026-07-08 22:35:19 +0900

    토큰 갱신 API 추가 (simplejwt TokenRefreshView)

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
 from .views import SignupView, LoginView
 
 urlpatterns = [
     path("signup", SignupView.as_view()),
     path("login", LoginView.as_view()),
+    path("refresh", TokenRefreshView.as_view()),
 ]
